5e25aa2fb3e8fc21a7d78143fff669628e085f81
Matej Kriz <matej@matejkriz.cz>
2021-11-24 14:54:29 +0100

    feat(early-access): when user disable early access, display confirmation modal with link to download stable release

--- packages/suite-desktop/src/support/DesktopUpdater/EarlyAccessDisable.tsx
+++ packages/suite-desktop/src/support/DesktopUpdater/EarlyAccessDisable.tsx
@@ -1,20 +1,23 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import { Button } from '@trezor/components';
-import { Translation, Modal, Image } from '@suite-components';
+import { Translation, Modal, Image, TrezorLink } from '@suite-components';
 import { ImageWrapper, ButtonWrapper, Description, LeftCol, RightCol, Title } from './styles';
+import { SUITE_URL } from '@suite-constants/urls';
 
 interface Props {
     hideWindow: () => void;
 }
 
 const EarlyAccessDisable = ({ hideWindow }: Props) => {
+    const [enabled, setEnabled] = useState(true);
+
     const allowPrerelease = useCallback(() => {
         window.desktopApi?.allowPrerelease(false);
-        hideWindow();
-    }, [hideWindow]);
+        setEnabled(false);
+    }, []);
 
-    return (
+    return enabled ? (
         <Modal>
             <ImageWrapper>
                 <Image width={60} height={60} image="EARLY_ACCESS_DISABLE" />
@@ -36,6 +39,33 @@ const EarlyAccessDisable = ({ hideWindow }: Props) => {
                     <Button onClick={allowPrerelease} fullWidth>
                         <Translation id="TR_EARLY_ACCESS_DISABLE" />
                     </Button>
+                </RightCol>
+            </ButtonWrapper>
+        </Modal>
+    ) : (
+        <Modal>
+            <ImageWrapper>
+                <Image image="UNI_SUCCESS" />
+            </ImageWrapper>
+            <Title>
+                <Translation id="TR_EARLY_ACCESS_LEFT_TITLE" />
+            </Title>
+            <Description>
+                <Translation id="TR_EARLY_ACCESS_LEFT_DESCRIPTION" />
+            </Description>
+
+            <ButtonWrapper>
+                <LeftCol>
+                    <Button onClick={hideWindow} variant="secondary" fullWidth>
+                        <Translation id="TR_EARLY_ACCESS_SKIP_REINSTALL" />
+                    </Button>
+                </LeftCol>
+                <RightCol>
+                    <TrezorLink size="small" variant="nostyle" href={SUITE_URL}>
+                        <Button icon="EXTERNAL_LINK" alignIcon="right" fullWidth>
+                            <Translation id="TR_EARLY_ACCESS_REINSTALL" />
+                        </Button>
+                    </TrezorLink>
                 </RightCol>
             </ButtonWrapper>
         </Modal>
